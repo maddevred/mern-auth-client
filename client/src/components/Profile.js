3d9429fb0168c34eae37349f3e81446bf503f385
Amari James <mar.james1998@gmail.com>
2020-12-18 12:42:05 -0500

    addingstuff

---
+++ client/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <p>This is Navbar us...</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Tempore dolorum excepturi, pariatur dolore fuga perferendis 
+                mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
+                dolorem tempora possimus accusantium, fugit eius quidem?
+            </p>
+        </div>
+    )
+}
+
+export default Navbar;
